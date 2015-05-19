a873039869221f21cbcbb4bd5526e880fb51ce2d
Vladimir Strackovski <vlado@nv3.org>
2015-05-19 12:42:35 +0200

    Enable collapsed sidebar slider

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -5,4 +5,41 @@ $(document).ready(function () {
         slideshow: true,
         timer: 2000
     });
+
+    if ($(window).width() < 767) {
+        $('.latest-slider').nvslider({
+            showItems: 1,
+            slideFor: 1,
+            slideshow: true,
+            timer: 6000
+        });
+
+        $('.news-slider').nvslider({
+            showItems: 1,
+            slideFor: 1,
+            slideshow: true,
+            timer: 6000
+        });
+    }
+});
+
+$(window).resize(function(){
+    if ($(window).width() < 767) {
+        $('.latest-slider').nvslider({
+            showItems: 1,
+            slideFor: 1,
+            slideshow: true,
+            timer: 6000
+        });
+
+        $('.news-slider').nvslider({
+            showItems: 1,
+            slideFor: 1,
+            slideshow: true,
+            timer: 6000
+        });
+    } else {
+        $('.latest-slider').nvslider('destroy');
+        $('.news-slider').nvslider('destroy');
+    }
 });
